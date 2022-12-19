6787da183517afe0adb80b2b5a1c43196ae9716c
Nghĩa Nef <nghia14302@gmail.com>
2022-12-19 22:37:47 +0700

    ADD - eslint unused imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,12 @@ module.exports = {
                 endOfLine: 'auto',
             },
         ],
+        'no-unused-var': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': [
+            'warn',
+            { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+        ],
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
