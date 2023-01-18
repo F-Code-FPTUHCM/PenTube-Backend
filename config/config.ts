19541c3fba7b4d20ecb43a3e11bb74175524c723
nghia <nghia14302@gmail.com>
2023-01-18 11:46:43 +0700

    Create dev environment for deployment

--- config/config.ts
+++ config/config.ts
@@ -4,7 +4,7 @@ import { join } from 'path';
 
 // Get .env from yaml file
 export const configYAML = () => {
-    const YAML_CONFIG_URI = 'config.yaml';
+    const YAML_CONFIG_URI = 'config.dev.yaml';
     return yaml.load(readFileSync(join(__dirname, YAML_CONFIG_URI), 'utf8')) as Record<string, any>;
 };
 
